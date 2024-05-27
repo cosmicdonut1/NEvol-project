725513f9756790a4121a63f7c77952ad8a10d29f
Venkat <akash1209@gmail.com>
2024-05-27 23:40:14 +0200

    added a config file for easy execution

---
+++ config.py
@@ -0,0 +1,24 @@
+# Venkat's Unicorn
+device_details = dict(
+    id="UN-2023.04.61",
+    sfreq=125,
+    total_channel_from_device = 17,
+    channels=["Fz", "C3", "Cz", "C4", "Pz", "PO7", "Oz", "PO8"],
+)
+epoch_information = dict(
+    # buffer size in seconds
+    duration=2,
+)
+
+# Kseniia's OpenBCI
+# device_details = dict(
+#     id="UN-2023.04.61",
+#     sfreq=125,
+#     total_channel_from_device = 17,
+#     channels=["Fz", "C3", "Cz", "C4", "Pz", "PO7", "Oz", "PO8"],
+# )
+# epoch_information = dict(
+#     # buffer size in seconds
+#     duration=2,
+# )
+
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 # import pandas as pd
 from plot import plotEEGData
 from analyze import analyze_signal, classify_eyeblinks
-
+import config
 
 if __name__ == "__main__":
     # Initialize the variables
@@ -17,19 +17,22 @@ if __name__ == "__main__":
     mode = "train"
     # mode = "predict"
 
-    epoch_duration = 2
-    sampling_rate = 125
-    channel_names = ["Fz", "C3", "Cz", "C4", "Pz", "PO7", "Oz", "PO8"]
+    epoch_duration = config.epoch_information['duration']
+    sampling_rate = config.device_details['sfreq']
+    channel_names = config.device_details['channels']
+    all_channels = config.device_details['total_channel_from_device']
+    device_id = config.device_details['id']
+
     ls_markers = []
     eeg_signals = np.zeros((len(channel_names), epoch_duration * sampling_rate))
 
-    buffer = Buffer(duration=epoch_duration, sampling_rate=sampling_rate, num_channels=17)
+    buffer = Buffer(duration=epoch_duration, sampling_rate=sampling_rate, num_channels=all_channels)
     stop_event = Event()
 
-    bool_s_stream_status = check_stream("UN-2023.04.61")
+    bool_s_stream_status = check_stream(device_id)
 
     if bool_s_stream_status:
-        signal_thread = Thread(target=read_signal_stream, args=("UN-2023.04.61", buffer, stop_event))
+        signal_thread = Thread(target=read_signal_stream, args=(device_id, buffer, stop_event))
         signal_thread.start()
 
         while True:
